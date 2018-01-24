254cb602c5134934b6f1f18cbb2e0bc5af322156
VictoriaBelina <viky2296@gmail.com>
2018-01-24 19:45:04 +0000

    post.php edited online with Bitbucket

--- post.php
+++ post.php
@@ -6,8 +6,16 @@ $q = $_GET['Tests'];
 if( isset($_GET['Tests'])) {
 	$test_id = (int)$_GET['Tests'];
 	$test_data = get_test_data($test_id);
-	var_dump($test_data);
-
+	//var_dump($test_data);
+	<ul>
+	foreach ($test_data as $key => $value)
+	{
+		foreach($value as $v)
+		{
+			<li>$v</li>
+		}
+	}
+	</ul>
 }
 
 ?>
